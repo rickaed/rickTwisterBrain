c8adc30591692979ee5f86769b61c0ef184f8f04
CouffignalCedric <cedric.couffignal.sncf@gmail.com>
2023-01-19 16:52:40 +0100

    Merge branch 'dev' of github.com:ahmidev/TwisterBrain into cedric_c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayerComponent } from './player/player.component';
 import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
