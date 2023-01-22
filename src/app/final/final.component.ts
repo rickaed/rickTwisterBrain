0f2f581d93992adef672e18a363ca8d74e804c6a
SandraConil <[sandra.conil@sncf.fr](mailto:sandra.conil@sncf.fr)>
2023-01-22 15:44:37 +0100

    test ts pour les joeurs

--- src/app/final/final.component.ts
+++ src/app/final/final.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./final.component.css']
 })
 export class FinalComponent implements OnInit {
-  
+joueurs = [
+  {'id': 1,
+'image': assets/avatar1.svg,
+'score': 12
+ },
+ {'id': 2,
+ 'image': assets/avatar2.svg,
+ 'score': 256
+  },
+  {'id': 3,
+  'image': assets/avatar.3svg,
+  'score': 25656
+   },
+   {'id': 4,
+   'image': assets/avatar4.svg,
+   'score': 125
+  }
+]
 
   constructor(){}
   ngOnInit(): void {
+
     
   }
 }
