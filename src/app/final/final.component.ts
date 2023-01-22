d0134064a008244dd9fa3118f0d09d0df7dc3e40
SandraConil <[sandra.conil@sncf.fr](mailto:sandra.conil@sncf.fr)>
2023-01-22 14:56:01 +0100

    mode 1 joueur

--- src/app/final/final.component.ts
+++ src/app/final/final.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-final',
   templateUrl: './final.component.html',
   styleUrls: ['./final.component.css']
 })
-export class FinalComponent {
-
+export class FinalComponent implements OnInit {
   
+
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
 }
