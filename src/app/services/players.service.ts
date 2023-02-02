984ed6b2cf69d3c1197d404ab929ab78ed237c01
SandraConil <[sandra.conil@sncf.fr](mailto:sandra.conil@sncf.fr)>
2023-02-02 18:37:47 +0100

    test score final

--- src/app/services/players.service.ts
+++ src/app/services/players.service.ts
@@ -78,19 +78,19 @@ export class PlayersService {
 
     // );
 
-    // tableau.forEach(element => console.log(element));
-    // si le score P1 est > P2 P3 P4 || 
+  //   tableau.forEach(element => console.log(element));
+  //   // si le score P1 est > P2 P3 P4 || 
   // if (this.players[0].score > this.players[1].score && this.players[2].score && this.players[3].score) {
-    //   console.log(this.players[0])
+  //  console.log(this.players[0])
       
-    // }else if (this.players[1].score > this.players[0].score && this.players[2].score && this.players[3].score) {
-    //   console.log(this.players[1])
+  //    }else if (this.players[1].score > this.players[0].score && this.players[2].score && this.players[3].score) {
+  //    console.log(this.players[1])
      
-    // }else if (this.players[2].score > this.players[0].score && this.players[1].score && this.players[3].score) {
-    //   console.log(this.players[2])
+  //    }else if (this.players[2].score > this.players[0].score && this.players[1].score && this.players[3].score) {
+  //     console.log(this.players[2])
       
-    // }else if (this.players[3].score > this.players[0].score && this.players[2].score && this.players[1].score) {
-    //   console.log(this.players[3])
+  //    }else if (this.players[3].score > this.players[0].score && this.players[2].score && this.players[1].score) {
+  //      console.log(this.players[3])
      
     // }
     // return winner
