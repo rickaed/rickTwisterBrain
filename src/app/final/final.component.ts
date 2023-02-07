693d00fe17d7ec0d07c433d4f43b38b77672833f
SandraConil <[sandra.conil@sncf.fr](mailto:sandra.conil@sncf.fr)>
2023-02-07 21:22:56 +0100

    confettis ok

--- src/app/final/final.component.ts
+++ src/app/final/final.component.ts
@@ -1,4 +1,5 @@
-// const confettis = require('canvas-confetti');
+//pour importer mon API canvas confetti
+const confettis = require('canvas-confetti');
 import { Component, forwardRef, Inject, OnInit } from '@angular/core';
 import { PlayersService } from '../services/players.service';
 import { Player } from '../models/player-model';
@@ -9,56 +10,32 @@ import {ConfettisService} from '../confettis.service';
   selector: 'app-final',
   templateUrl: './final.component.html',
   styleUrls: ['./final.component.css'],
-  
 })
-export class FinalComponent implements OnInit {
-  
-    // players:Player[]= this.playersService.players;
-    // btnDelet = false;
 
-// constructor(private playersService: PlayersService)  {}
-constructor (public confettisService: ConfettisService) {}
+
+export class FinalComponent implements OnInit {
+  //tableau pour mes joueurs
+    players:Player[]= this.playersService.players;
+
+    btnDelet = false;
+
+constructor (public confettisService: ConfettisService, private playersService: 
+  PlayersService) {}
+
 
 ngOnInit(){
   //pour afficher mon API
-  this.getConfettis();
+  this.showConfetti();
 }
-
-  // ngOnInit(): void {
-    
-  //  console.log(this.playersService.players)
-
-  //  const myCanvas = document.createElement('canvas');
-  //  document.body.appendChild(myCanvas);
-
-  //   const myConfetti = confettis.create(myCanvas, {resize: true, useWorker:true});
-  //   myConfetti({
-  //     particleCount: 5000,
-  //     spread: 10240,
-  //    propagation: 5260,
-  //    angle: 90,
-  //    startVelocity: 40,
-  //    drift : -5, 
- 
-  //   });
-  // }
-
-  // showConfetti() {
-  //   const myConfetti = confettis.create();
-  //   myConfetti({
-  //     particleCount: 1000,
-  //     spread: 560,
-  //     propagation: 1589
-
-    
-  //   });
-  // }
-getConfettis(){
-  // console.log('Execution API')
-  return fetch (" https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js ")
-   .then (response => response.json)
-   .then(confettis => this.confettisService.setConfettis(confettis));
- 
-}
+  
+  showConfetti() {
+  let canvas = document.querySelector('.canvas') as any;
+  let confetti = confettis.create(canvas, { resize: true });
+  confetti({
+    spread: 170,
+   origin: { y: 0.5 }
+   
+  });
+  }
 
 }
